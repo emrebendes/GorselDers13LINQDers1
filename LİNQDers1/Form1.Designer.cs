b7c2c0cdc7b44dbf2413b9f3bffc846ddec3eb47
EMRE5 <emrebendes@gmail.com>
2022-06-02 17:04:44 +0300

    Add project files.

---
+++ LİNQDers1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace LİNQDers1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 505);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(816, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(747, 504);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1598, 594);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ LİNQDers1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LİNQDers1
+{
+    public partial class Form1 : Form
+    {
+        public List<Product> products = new List<Product>()
+        {
+            new Product() { id =1, productName ="Acer",price=10000,categoryId=1 },
+            new Product() { id =2, productName ="HP",price=55000,categoryId=1 },
+            new Product() { id =3, productName ="Lenovo",price=32000,categoryId=1 },
+            new Product() { id =4, productName ="Samsung",price=15000,categoryId=2 },
+            new Product() { id =5, productName ="Iphone",price=20000,categoryId=2 },
+            new Product() { id =6, productName ="Asus",price=1520,categoryId=2 },
+            new Product() { id =6, productName ="Asus",price=12000,categoryId=1 }
+        };
+        public List<Category> categories = new List<Category>()
+        {
+            new Category() { categoryId = 1, categoryName ="Computer"},
+            new Category() { categoryId = 2, categoryName ="Phone"}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            int a = 5;
+            //MessageBox.Show("f =" + a.faktoriyel());
+
+            // List<Product> p =  priceLimit(9000);
+
+            //IEnumerable<Product>
+            /*var products1 = products.
+                Select(p => new { p.productName, p.price }).
+                Where(p => p.price > 9000);*/
+
+            /*var a =(from p in products 
+                            where p.price > 9000
+                            select new { p.productName, p.price })*/
+
+            /*dataGridView1.DataSource = (from p in products
+                    join c in categories
+                    on p.categoryId equals c.categoryId
+                    select new {p.productName, p.price,c.categoryName}).ToList();*/
+
+            var aa = from p in products
+                    group p by p.categoryId into g
+                    select new { g.Key, adet = g.Count(), grup=g };
+
+            foreach (var c in aa)
+            {
+                listBox1.Items.Add("kategori :"+c.Key+" toplam ürün : "+c.adet);
+                foreach (var d in c.grup)
+                    listBox1.Items.Add(d.productName);
+            }
+
+                  
+
+            //dataGridView1.DataSource=products1.ToList();
+            /* foreach (var product in products1)
+             {
+                 listBox1.Items.Add(product.productName);
+             }*/
+
+
+
+        }
+
+
+        public List<Product> priceLimit(double a)
+        {
+            List<Product> p = new List<Product>();
+            foreach (Product product in products)
+            { 
+                if (product.price>=a)
+                    p.Add(product);
+            }
+            return p;
+        }
+    }
+
+    public class Product
+    {
+        public int id { get; set; }
+        public string productName { get; set; }
+        public double price { get; set; }
+        public int categoryId { get; set; }
+
+    }
+
+    public class Category
+    {
+        public int categoryId { get; set; }
+        public string categoryName { get; set; }
+
+    }
+
+
+    public static class OurFuncs
+    {
+        public static int faktoriyel(this int n)
+        {
+            int a=1;
+            for (int i = n; i > 0; i--)
+            { 
+                a*=i;
+            }
+            return a;
+        }
+    }
+
+
+}
